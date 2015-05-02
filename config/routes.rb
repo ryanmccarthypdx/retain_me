eae4a3acfb8206de2587e763328b11fcffc564f4
Mike Perham <mperham@gmail.com>
2015-05-02 16:15:21 -0700

    Fancy landing page!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts, only: :create
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150502225852) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "basic_profiles", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
