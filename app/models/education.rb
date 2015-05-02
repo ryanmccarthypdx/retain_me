3205d0c4d0d3c9a3b84ecb29aecee11891ddc188
jasandine <jasandine@gmail.com>
2015-05-02 16:02:43 -0700

    update db schema

---
+++ app/models/education.rb
@@ -0,0 +1,3 @@
+class Education < ActiveRecord::Base
+  belongs_to :full_profile
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502225852) do
+ActiveRecord::Schema.define(version: 20150502230059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 20150502225852) do
     t.datetime "updated_at",         null: false
   end
 
+  create_table "educations", force: :cascade do |t|
+    t.string   "school_name"
+    t.string   "field_of_study"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "degree"
+    t.string   "activities"
+    t.string   "notes"
+    t.integer  "full_profile_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "full_profiles", force: :cascade do |t|
     t.string   "associations"
     t.string   "honors"
@@ -48,6 +61,18 @@ ActiveRecord::Schema.define(version: 20150502225852) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string   "title"
+    t.string   "summary"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.boolean  "is_current"
+    t.string   "company"
+    t.integer  "full_profile_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
