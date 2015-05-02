8be389e06f94b3afef93a8ec96071ef59728aa70
jasandine <jasandine@gmail.com>
2015-05-02 11:36:58 -0700

    Add basic cancan setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  Roles = [ :admin, :default ]
+
+  def is?( requested_role.to_s )
+    self.role == requested_role.to_s
+  end
 end
