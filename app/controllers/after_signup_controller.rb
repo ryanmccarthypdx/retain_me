242df0c2e7ab004fdf415476072e66f1394bab44
Ryan <ryanmccarthypdx@gmail.com>
2015-05-03 13:28:52 -0700

    Fix forms and routes

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -2,28 +2,22 @@ class AfterSignupController < ApplicationController
   before_filter :authenticate_user!
 
   def step_0
-    @user_basic_profile = current_user.basic_profile
-    render 'step_0'
   end
 
   def step_1
-    @user_basic_profile = current_user.basic_profile
-    binding.pry
-    render 'step_1'
+  end
+
+  def step_1_save
+    current_user.basic_profile.update_attributes(basic_profile_params)
+    redirect_to step_2_path
   end
 
   def step_2
-    @user_basic_profile = current_user.basic_profile
-    if @user_basic_profile.update(basic_profile_params)
-      render 'step_2'
-    end
   end
 
-  def save
-    @user_basic_profile = current_user.basic_profile
-    if @user_basic_profile.update(basic_profile_params)
-      redirect_to feed_path
-    end
+  def step_2_save
+    current_user.basic_profile.update_attributes(basic_profile_params)
+    redirect_to feed_path
   end
 
   private
