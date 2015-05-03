58303bdbe062d972fcc911a5ca22f9088d118ad0
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-02 19:44:44 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/linkedin' => 'linkedin#index'
   get '/linkedin_profile' => 'linkedin#linkedin_profile'
-  get '/oauth_account' => "linkedin#oauth_account"
+
+  get '/oauth_callback' => "linkedin#oauth_account"
   get '/linkedin_oauth_url' => 'linkedin#generate_linkedin_oauth_url'
 end
