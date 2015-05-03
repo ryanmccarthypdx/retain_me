594520e202798a541433ff17e8dcc3301c23099a
Mike Perham <mperham@gmail.com>
2015-05-03 11:50:28 -0700

    Style news feed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+mike = User.find_by_email("mperham@gmail.com")
+unless mike
+  mike = User.create! :email => "mperham@gmail.com", :password => 'password', :password_confirmation => 'password'
+end
+unless mike.basic_profile
+  BasicProfile.create! user_id: mike.id, formatted_name: 'Mike Perham', first_name: 'Mike', last_name: 'Perham', picture_url: 'http://www.mikeperham.com/images/head.jpg'
+end
